ca3b52eecb0893e7db1bb7a847cf07289d9f6332
Sajaysurya Ganesh <sajay.surya@gmail.com>
2018-07-18 18:15:59 +0100

    Add chain environment
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

---
+++ chain.py
@@ -0,0 +1,93 @@
+'''
+Simple gridworld as in http://ai.stanford.edu/~nir/Papers/DFR1.pdf
+
+Key for ascii art
+| - wall
+O - player location
+
+Has no goal state, episodes never end
+
+Reward:
+    see the paper for details
+'''
+
+import curses
+import numpy as np
+from pycolab import ascii_art
+from pycolab import human_ui
+from pycolab.prefab_parts import sprites as prefab_sprites
+
+
+GRID_WORLD = ['O....|']
+
+
+def make_game():
+    '''
+    builds the game and returns the engine
+    '''
+    return ascii_art.ascii_art_to_game(
+        GRID_WORLD,
+        what_lies_beneath='.',  # space character
+        sprites={'O': PlayerSprite})
+
+
+class PlayerSprite(prefab_sprites.MazeWalker):
+    '''
+    Class for player
+
+    Defines movements and associated rewards
+    '''
+
+    def __init__(self, corner, position, character):
+        '''
+        initialize as per superclass instructions and make walls impassable
+        '''
+        super(PlayerSprite, self).__init__(
+            corner, position, character, impassable='|')
+
+    def update(self, actions, board, layers, backdrop, things, the_plot):
+        del backdrop, things, layers  # Unused
+
+        # flip the actions with probability 0.2
+        if np.random.random() < 0.2:
+            if actions == 0:
+                actions = 1
+            elif actions == 1:
+                actions = 0
+
+        if actions == 0:    # go east (mostly)
+            the_plot.add_reward(0.0)
+            obstruction = self._east(board, the_plot)
+            if obstruction == '|':
+                the_plot.add_reward(10)
+        elif actions == 1:  # go west (mostly)
+            the_plot.add_reward(2.0)
+            self._teleport((0, 0))
+        elif actions == 4:  # stay (for human player)
+            self._stay(board, the_plot)
+        if actions == 5:    # quit (for termination)
+            the_plot.terminate_episode()
+
+
+def main():
+    '''
+    C-style main function
+    '''
+
+    # Build the game
+    game = make_game()
+
+    # Create user interface
+    user_interface = human_ui.CursesUi(
+        keys_to_actions={curses.KEY_LEFT: 1,
+                         curses.KEY_RIGHT: 0,
+                         -1: 4,  # for dummy stayput action
+                         'q': 5, 'Q': 5},  # to exit
+        delay=200)
+
+    # Run the game
+    user_interface.play(game)
+
+
+if __name__ == '__main__':
+    main()
